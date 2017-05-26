ba4d9154ff4d7cb1a2e7ce2fd34f7ad445f0d0c7
Art Rodriguez <art.rodriguez@live.com>
2017-05-26 12:28:49 -0400

    add signup feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
 
   get '/login' do
   	if logged_in?
-  		redirect to '/your'
+  		redirect to '/your-food'
   	else
   		flash[:message] = "Please log in."
   		erb :'users/login'
@@ -36,15 +36,40 @@ class ApplicationController < Sinatra::Base
   	end
   end
 
+   get '/signup' do
+  	if logged_in?
+  		redirect to '/your-food'
+  	else
+  		erb :'users/signup'
+  	end
+  end
+
+  post '/signup' do
+  	if params[:username] == "" || params[:email] == "" || params[:password] == ""
+  		flash[:message] = "Please fill out every field."
+      redirect '/signup'
+    else
+    	user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    	user.save
+    	session[:user_id] = user.id
+    	flash[:message] = "Your account was successfully created! Woot!"
+  		redirect to '/your-food'
+  	end
+  end
+
   get '/logout' do
   	if logged_in?
   		session.clear
-  		flash[:message] = "Please log in."
+  		flash[:message] = "Your are now logged out.  Thank you!"
   		redirect to '/'
   	else
   		flash[:message] = "Please sign up or log in."
   		redirect to '/'
   	end
+  end
+
+  get '/your-food' do
+  	erb :'your-food'
   end
 
   helpers do
