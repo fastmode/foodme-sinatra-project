7dba3ba4e98bf73c1b15e741b549fb36e65fa70c
Art Rodriguez <art.rodriguez@live.com>
2017-05-27 18:49:24 -0400

    added feature to edit existing review

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -104,12 +104,34 @@ class ApplicationController < Sinatra::Base
     erb :'reviews/show-review'
   end
 
-  patch '/reviews/:id' do
-
+  get '/reviews/:id/edit' do
+    if logged_in?
+      @review = Review.find_by_id(params[:id])
+      if @review.user_id == current_user.id
+        erb :'reviews/edit-review'
+      else
+        redirect to '/reviews'
+      end
+    else
+      redirect to '/login'
+    end
   end
 
-
-
+  patch '/reviews/:id' do
+    @review = Review.find_by_id(params[:id])
+    @review.dish.name = params[:dish][:name]
+    @review.dish.price = params[:dish][:price]
+    @review.dish.gluten_free = params[:dish][:gluten_free]
+    @review.dish.vegetarian = params[:dish][:vegetarian]
+    @review.content = params[:content]
+    @review.dish.restaurant.name = params[:restaurant]
+    @review.dish.restaurant.city.name = params[:city]
+    @review.save
+    @review.dish.save
+    @review.dish.restaurant.save
+    @review.dish.restaurant.city.save
+    redirect to "/reviews/#{@review.id}"
+  end
 
 
   helpers do
